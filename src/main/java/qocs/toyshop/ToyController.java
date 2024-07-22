e7b6e2c20847ea17a150674fb51a6870374ab617
Qocs <ahdehfl1004@gmail.com>
2024-07-22 23:25:25 +0900

    HomeController

---
+++ src/main/java/qocs/toyshop/ToyController.java
@@ -0,0 +1,17 @@
+package qocs.toyshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+
+@Controller
+public class ToyController {
+
+    @GetMapping("toy")
+    public String toy(Model model) {
+        model.addAttribute("data", LocalDate.now());
+        return "home";
+    }
+}
